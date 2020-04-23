2e335d72005a3dbfc250cc2db924da5381c7d2ca
imane <imsaoubi@enseirb-matmeca.fr>
2020-04-23 18:46:54 +0200

    codes php permettant la connexion et la deconnexion/ hmlt et css  de la page de connexion.

---
+++ code/connexion.php
@@ -0,0 +1,32 @@
+<?php
+
+include("create_database.php");
+include("create_utilisateur.php");
+
+  if(isset($_POST['email']) && isset($_POST['pwd'])){
+    if(!empty($_POST['email']) || !empty($_POST['pwd'])){
+      $connexion = mysqli_connect("localhost","admin","it103","louda");
+      if(!$connexion){
+      die("erreur de connexion à la base de donnée");}
+
+      $email = $_POST['email'];
+      $pwd = $_POST['pwd'];
+
+      $sql = "SELECT count(*) FROM  utilisateur where email = '".$email."' AND mot_de_passe = '".$pwd."' ";
+    $result = mysqli_query($connexion,$sql);
+
+      $row = mysqli_fetch_array($result);
+      $count = $row['count(*)'];
+      if($count!=0){
+
+      $SESSION['email']=$_POST['email'];
+      $SESSION['pwd']=$_POST['pwd'];
+
+  header("location: inscription.php");}
+else {
+      header("location: connexion_page.php?erreur=1");}}}
+
+mysqli_close($connexion);
+
+
+?>
---
+++ code/connexion_page.php
@@ -0,0 +1,67 @@
+
+
+<!DOCTYPE html>
+<html lang ="fr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Page de connexion </title>
+  </head>
+<body>
+  <div class="container">
+    <div class="row ">
+      <div class="col-8">
+        <h1>Titre du site</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class= "col-md-4 offset-md-8"> Pas encore inscrit ? <a href ='inscription.php'> inscription</a></div>
+    </div>
+
+    <div class="jumbotron" style="margin-top:60px">
+    <div class=" row">
+
+      <div class= "col-md-4 offset-md-5"> Se connecter </div>
+    </div>
+
+
+    <form action= "connexion.php" method="post" >
+      <div class="form-group">
+
+      <label> Email : </label>
+      <input type="text" class ="form-control" name="email" required  placeholder="Email">
+
+
+
+      <label>Mot de passe :</label>
+      <input type ="password" class="form-control" name="pwd" required placeholder="Mot de passe">
+
+         <br>
+       <input type ="submit" value="Confirmer">
+<?php
+
+if(isset($_GET['erreur'])){
+  $err = $_GET['erreur'];
+  if($err = 1){
+  ?>  <div class="alert alert-danger" role = "alert">Vos identifiants sont incorrectes.</div> <?php
+  }
+  }
+
+?>
+      </div>
+    </form>
+  </div>
+  </div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
+
+  </html>
